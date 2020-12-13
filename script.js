acbd87f6349bf4f7dbd555b2e7ec9f93c20c2fe8
Mona Lisa <clarkefraser1@gmail.com>
2020-12-13 15:27:17 +1100

    Modifying html timeblocks to 24hr time, Adding local storage to js file

--- script.js
+++ script.js
@@ -14,65 +14,83 @@ WHEN I refresh the page
 THEN the saved events persist
 */
 
-//create current time.
-var dateTime = moment().format('MMMM Do YYYY, h:mm a');
-$("#currentDay").append(dateTime);
+$(document).ready(function (){
+
+	//create current time.
+	// var dateTime = moment().format('MMMM Do YYYY, h:mm a');
+	var dateTime = moment().hours();
+	console.log(dateTime);
+	// $("#currentDay").append(dateTime);
+
+    //Logging text from savebtn, to console, then saving to local storage.
+
+	$(".saveBtn").on("click", function(){
+		// console.log($(this));
+		var inputText = $(this).siblings(".description").val();
+		console.log(inputText);
+		var currentHour = $(this).parent().attr("id");
+		console.log(currentHour);
+
+		localStorage.setItem(currentHour, inputText);
+
+	});
+
+	// var storage
+	// $("textarea").on("click", function(event){
+	    
+	//     
+	//     console.log();
+	// });
 
 
-var storage
-$("textarea").on("click", function(event){
-    
-    localStorage.setItem("storage", JSON.stringify(storage));
-    console.log();
+	/*$("textarea").on("click", function(event){
+	event.append(console.log);
+	})
+	console.log();
+	/*
+
+	// Finish up and make a completed HTML timeblock
+
+	// i.e
+	//    <div id="hour-9" class="row time-block">
+	//        <div class="col-md-1 hour">
+	//          9AM
+	//        </div>
+	//        <textarea class="col-md-10 description"></textarea>
+	//        <button class="btn saveBtn col-md-1"><i class="fas fa-save"></i></button>
+	//    </div>
+
+	// Use javascript to make what is written in the <textarea> appear in the console when the button is clicked
+
+	// Use javascript to make what is written in the <textarea> appear in the local storage
+
+	// Use javascript to pull what is in the local storage and insert it into the corresponding time block
+
+	// Use javascript to color time blocks according to local time
+
+
+	/*var past = < dateTime;
+	var present = dateTime;
+	var future = >datTime;
+
+	for (past) function {
+	    if past 
+	}
+	/*
+
+	//variables past present future
+
+
+	/*if hours9 >dateTime
+	block text , change color
+	}
+
+	//color coded blocks past present future.... if datetime <.....or if datetime true/false
+
+	//cannot acess block if past a certain time
+
+	//when clicking time block onclick envent lister
+
+	//save to local storage*/
+
 });
-
-
-/*$("textarea").on("click", function(event){
-event.append(console.log);
-})
-console.log();
-/*
-
-// Finish up and make a completed HTML timeblock
-
-// i.e
-//    <div id="hour-9" class="row time-block">
-//        <div class="col-md-1 hour">
-//          9AM
-//        </div>
-//        <textarea class="col-md-10 description"></textarea>
-//        <button class="btn saveBtn col-md-1"><i class="fas fa-save"></i></button>
-//    </div>
-
-// Use javascript to make what is written in the <textarea> appear in the console when the button is clicked
-
-// Use javascript to make what is written in the <textarea> appear in the local storage
-
-// Use javascript to pull what is in the local storage and insert it into the corresponding time block
-
-// Use javascript to color time blocks according to local time
-
-
-/*var past = < dateTime;
-var present = dateTime;
-var future = >datTime;
-
-for (past) function {
-    if past 
-}
-/*
-
-//variables past present future
-
-
-/*if hours9 >dateTime
-block text , change color
-}
-
-//color coded blocks past present future.... if datetime <.....or if datetime true/false
-
-//cannot acess block if past a certain time
-
-//when clicking time block onclick envent lister
-
-//save to local storage*/
